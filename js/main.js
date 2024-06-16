553b7d94f04316860de0669ab448c0cd7f9efa0c
Henrique Gamborgi <henriquegamborgi@gmail.com>
2024-06-16 18:01:56 -0400

    Update Headers and Footers
    In this commit, initial HTML and CSS was developed for header and footer.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 console.log("JavaScript file is linked.");
+
+const toggleButton = document.querySelector("#button")
+const navbarLinks = document.querySelector(".links-header")
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
